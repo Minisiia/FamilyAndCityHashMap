32846a32236037f5816bf98412f5e3c2c7f22de1
Minisiia <ar_tik@yahoo.com>
2022-12-18 20:45:42 +0200

    Java Advanced 2. Collections

--- src/family_and_city/FamilyAndCity.java
+++ src/family_and_city/FamilyAndCity.java
@@ -5,15 +5,27 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class FamilyAndCity {
-    public static void main(String[] args) {
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("Москва", "Іванови");
-        hashMap.put("Київ", "Петрови");
-        hashMap.put("Лондон", "Абрамовичі");
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть назву міста:");
-        String city = scanner.nextLine();
+        HashMap<String, String> hashMap = new HashMap<>();
+        String keyCity;
+        String valueSurname;
         boolean flag = false;
+        for (;;) {
+            System.out.println("Введіть назву міста:");
+            keyCity = scanner.nextLine();
+            System.out.println("Введіть призвище:");
+            valueSurname = scanner.nextLine();
+            hashMap.put(keyCity, valueSurname);
+            System.out.println("Ввести наступні місто та призвище? y/n");
+            char answer = scanner.nextLine().charAt(0);
+            if (answer == 'n') break;
+            else if (answer != 'y') throw new Exception("Немає такого варіанту вибору");
+        }
+
+        System.out.println("Введіть для пошуку назву міста:");
+        String city = scanner.nextLine();
+
         for (Map.Entry<String, String> temp : hashMap.entrySet()) {
             if (city.equals(temp.getKey())) {
                 System.out.println(temp.getValue());
